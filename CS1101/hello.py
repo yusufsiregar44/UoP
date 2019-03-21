b1cac4f70f3a531698c7abb1e0ccfd03ea695eb9
yusufsiregar44 <yususiregar5@gmail.com>
2019-03-21 21:16:01 +0700

    add latest work

--- CS1101/hello.py
+++ CS1101/hello.py
@@ -1,6 +1,11 @@
-x = 12/60 * 100
-print(x)
-
-# x = x * 3
-
-# print(y) 
+mylist = [ [2,4,1], [1,2,3], [2,3,5] ]
+a=0
+b=0
+total = 0
+while a <= 2:
+    while b < 2:
+        total += mylist[a][b]
+        b += 1
+    a += 1
+    b = 0  
+print (total)
---
+++ CS1101/wc.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    print(linecount('wc.py'))
+
+def linecount(filename):
+    count = 0
+    for line in open(filename):
+        count += 1
+    return count
+
+# print(linecount('wc.py'))
