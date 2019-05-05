33dd55010962e84dcf8a994e53429d3b0aa3a962
yusufsiregar44 <yususiregar5@gmail.com>
2019-05-05 21:27:57 +0700

    feat: add exploration of variables

---
+++ CS1102/1/VariablesAndPrimitiveTypes.java
@@ -0,0 +1,11 @@
+public class VariablesAndPrimitiveTypes {
+  public static void main (String[] args) {
+    char ch = ' ';
+    // float a = 3;
+    double m = 3;
+    String a = "jahah";
+
+    // a.length();
+    System.out.println(9.0 * 7.98);
+  }
+}
