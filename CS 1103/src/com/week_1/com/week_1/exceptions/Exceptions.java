ddc3d716c928adead28827980b5f46a0f31b0e5e
yusufsiregar44 <yususiregar5@gmail.com>
2020-08-16 20:42:28 +0700

    Add CS 1103 Week 1 Lab 2 files

---
+++ CS 1103/src/com/week_1/com/week_1/exceptions/Exceptions.java
@@ -0,0 +1,51 @@
+package com.week_1.com.week_1.exceptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+/**
+ * @author Yusuf
+ *
+ * This class is designed to test Java's Exception
+ */
+public class Exceptions {
+    InputStream in = null;
+    OutputStream out = null;
+    String urlString;
+    String fileString;
+    URL link = null;
+    Byte bytes = null;
+
+    public Exceptions(String url, String file) {
+        urlString = url;
+        fileString = file;
+
+        try {
+            assert urlString != null;
+            link = new URL(urlString);
+            in = link.openStream();
+
+            assert fileString != null;
+            out = new FileOutputStream(file);
+
+            copyStream(in, out);
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyStream(InputStream in, OutputStream out)
+        throws IOException {
+            int oneByte = in.read();
+            while (oneByte >= 0) { // negative value indicates end-of-stream
+                out.write(oneByte);
+                oneByte = in.read();
+             }
+        }
+}
---
+++ CS 1103/src/com/week_1/com/week_1/exceptions/ExceptionsMain.java
@@ -0,0 +1,13 @@
+package com.week_1.com.week_1.exceptions;
+
+/**
+ * @author Yusuf
+ */
+
+public class ExceptionsMain {
+
+    public static void main(String[] args){
+        Exceptions testException = new Exceptions("http://tutorials.jenkov.com/java-io/outputstream.html",
+                "/Users/useradmin/Desktop/apakek.html");
+    }
+}
